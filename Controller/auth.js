fb5e847466f9859130c9786fddc95934e5240bbd
Avinash DV <19746263+avinashdv@users.noreply.github.com>
2024-10-21 21:43:59 +0530

    import bug resolved

--- Controller/auth.js
+++ Controller/auth.js
@@ -2,7 +2,7 @@ const {
   registerValidation,
   loginValidation,
 } = require("../validation/User/validation");
-const User = require("../Model/User");
+const User = require("../model/User");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const dotenv = require("dotenv");
